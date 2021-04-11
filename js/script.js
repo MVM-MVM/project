32917910c19ea8cb3b528f66d61dcfaa4d598fc4
MVM <mvm.ster@gmail.com>
2021-04-11 09:54:34 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+"use strict";
+/*
+console.log("Makabra");
+
+const min = Number.MIN_SAFE_INTEGER;
+const max = Number.MAX_SAFE_INTEGER;
+console.log(min);
+console.log(max);
+
+let object1 = {
+    Name: "Wer",
+    Olde: 26,
+    isma: true
+};
+
+console.log(object1.Name);
+*/
+
+
+// const result = confirm("bla bla lba?");
+// console.log(result);
+
+
+// const result = prompt("bla bla lba?","23");
+// console.log(typeof(result));
+// console.log(result);
+
+// const answers = [];
+// answers[0] = prompt('bla bla 1?','23');
+// answers[1] = prompt('bla bla 2?','23');
+// answers[2] = +prompt('bla bla num1?','23');
+
+// document.write(answers);
+
+const bla = ['wew','pem',12,true,'35'];
+const fre = 24;
+console.log(`${bla[2]}` + `${fre}`);
+console.log(bla[2] + fre);
+console.log(+bla[4] + fre);
+console.log(+bla[3] + fre);
